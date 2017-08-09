ebe8bd9827a458d5faf64477e26403ae05cc8176
tim degroot <tdegroot4390@gmail.com>
2017-08-09 11:52:35 +0100

    added show and index

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+CAT = ["chinese", "italian", "japanese", "french", "belgian"]
+
+5.times do
+  res = Restaurant.new(name: Faker::Company.name, address: Faker::Address.street_address, category: CAT.sample, phone_number: Faker::PhoneNumber.cell_phone  )
+  if res.valid?
+    res.save
+  end
+end
+
+
